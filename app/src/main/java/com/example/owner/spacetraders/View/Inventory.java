fba998161da766fd33dcbdee76ca81192d722366
Kyle Rabinowiz <knr1008@gmail.com>
2019-03-07 14:23:44 -0500

    linked buttons to correct game status xml and to the inventory menu (to and from)

--- app/src/main/java/com/example/owner/spacetraders/View/Inventory.java
+++ app/src/main/java/com/example/owner/spacetraders/View/Inventory.java
@@ -1,7 +1,12 @@
 package com.example.owner.spacetraders.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.owner.spacetraders.R;
 
 public class Inventory extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class Inventory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.owner.spacetraders.R.layout.activity_inventory);
+
+        Button inventoryReturn = findViewById(R.id.inventory_return_button);
+
+        inventoryReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onInventoryReturnPressed(v);
+            }
+        });
     }
+
+    public void onInventoryReturnPressed(View view) {
+
+        startActivity(new Intent(Inventory.this, Game_Status.class));
+
+        finish();
+    }
+
 }
