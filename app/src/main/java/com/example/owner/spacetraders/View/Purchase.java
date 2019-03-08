5d94ac9d70870bb364fdab00b3eda3364beb5d94
Sam Milner <SRLMilner@aol.com>
2019-03-08 00:39:40 -0500

    Created 3 new xml files for the trading menu one as the main market, one for selling, one for buying

---
+++ app/src/main/java/com/example/owner/spacetraders/View/Purchase.java
@@ -0,0 +1,15 @@
+package com.example.owner.spacetraders.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.owner.spacetraders.R;
+
+public class Purchase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase);
+    }
+}
