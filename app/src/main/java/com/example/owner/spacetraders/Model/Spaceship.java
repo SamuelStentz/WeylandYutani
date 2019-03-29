0d8a09f9c9ee3504ac5aa36a7b533f5e2c253c5d
egu3334 <43274249+egu3334@users.noreply.github.com>
2019-03-28 22:30:39 -0400

    spaceship has fuel and its methods now

--- app/src/main/java/com/example/owner/spacetraders/Model/Spaceship.java
+++ app/src/main/java/com/example/owner/spacetraders/Model/Spaceship.java
@@ -2,13 +2,43 @@ package com.example.owner.spacetraders.Model;
 
 public class Spaceship {
     private SpaceshipType type;
+    private int currentFuel;
 
     public Spaceship() {
         type = SpaceshipType.GNAT;
+        currentFuel = type.getMaxFuel();
     }
 
     public int getCapacity() {
         return type.getCapacity();
+    }
+
+    public int getCurrentFuel() {
+        return currentFuel;
+    }
+
+    public int getMaxFuel() {
+        return type.getMaxFuel();
+    }
+
+    public int refill(int f) {
+        if(f > this.getMaxFuel() - this.getCurrentFuel()) {
+            return this.fullRefill();
+        }
+        currentFuel += f;
+        return f;
+    }
+
+    public int fullRefill() {
+        return refill(this.getMaxFuel() - this.getCurrentFuel());
+    }
+
+    public boolean loseFuel(int f) {
+        if (currentFuel - f < 0) {
+            return false;
+        }
+        currentFuel -= f;
+        return true;
     }
 
     enum SpaceshipType {
@@ -16,6 +46,7 @@ public class Spaceship {
 
         private String name;
         private int capacity;
+        private int maxFuel;
 
         SpaceshipType (String s, int c) {
             capacity = c;
@@ -24,6 +55,10 @@ public class Spaceship {
 
         public int getCapacity () {
             return capacity;
+        }
+
+        public int getMaxFuel() {
+            return maxFuel;
         }
 
         public String toString () {
